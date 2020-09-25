41ac8435b4772f87ef0dba99c51249b8da7a49c3
Bader Boland <bader@fairwinds.com>
2020-09-25 13:52:52 -0400

    Add GetPodSpec from Polaris

--- pkg/podspec/pod.go
+++ pkg/podspec/pod.go
@@ -1,7 +1,16 @@
 package podspec
 
-import "fmt"
+var podSpecFields = []string{"jobTemplate", "spec", "template"}
 
-func DoSomething() {
-	fmt.Println("vim-go")
+// GetPodSpec looks inside arbitrary YAML for a PodSpec
+func GetPodSpec(yaml map[string]interface{}) interface{} {
+	for _, child := range podSpecFields {
+		if childYaml, ok := yaml[child]; ok {
+			return GetPodSpec(childYaml.(map[string]interface{}))
+		}
+	}
+	if _, ok := yaml["containers"]; ok {
+		return yaml
+	}
+	return nil
 }
