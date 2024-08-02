4e9dd8b5e99515d563aeae835af7ace7dee7f311
jdesouza <james@fairwinds.com>
2024-08-02 10:14:56 -0300

    Added Controller validation

--- pkg/controller/pod.go
+++ pkg/controller/pod.go
@@ -16,12 +16,15 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 
+	"github.com/samber/lo"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var podSpecFields = []string{"jobTemplate", "spec", "template"}
+var controllerValidKinds = []string{"Deployment", "StatefulSet", "DaemonSet", "ReplicaSet", "CronJob", "Job"}
 
 // GetPodMetadataAndSpec looks inside arbitrary YAML for a PodSpec and it's metadata
 func GetPodMetadataAndSpec(obj map[string]any) (*metav1.ObjectMeta, *corev1.PodSpec, error) {
@@ -70,4 +73,50 @@ func getMetadata(parent map[string]any) (*metav1.ObjectMeta, error) {
 	var metadata metav1.ObjectMeta
 	err = json.Unmarshal(b, &metadata)
 	return &metadata, err
+}
+
+func ValidateIfControllerMatches(child map[string]any, controller map[string]any) error {
+	if child["metadata"].(map[string]any)["ownerReferences"].([]any)[0].(map[string]any)["uid"] != controller["metadata"].(map[string]any)["uid"] {
+		return fmt.Errorf("controller does not match ownerReference uid")
+	}
+	if child["metadata"].(map[string]any)["namespace"].(string) != controller["metadata"].(map[string]any)["namespace"].(string) {
+		return fmt.Errorf("controller namespace %s does not match ownerReference namespace %s", controller["metadata"].(map[string]any)["namespace"], child["metadata"].(map[string]any)["ownerReferences"].([]any)[0].(map[string]any)["namespace"])
+	}
+	if !lo.Contains(controllerValidKinds, controller["kind"].(string)) {
+		return fmt.Errorf("controller kind %s is not a valid controller kind", controller["kind"].(string))
+	}
+	childContainers := getChildContainers(child)
+	controllerContainers := getControllerContainers(controller)
+	if len(childContainers) != len(controllerContainers) {
+		return fmt.Errorf("length of controller container does not match child containers")
+	}
+	childContainerNames := lo.Map(childContainers, func(container any, _ int) string {
+		return container.(map[string]any)["name"].(string)
+	})
+	controllerContainerNames := lo.Map(controllerContainers, func(container any, _ int) string {
+		return container.(map[string]any)["name"].(string)
+	})
+	for _, childContainerName := range childContainerNames {
+		if !lo.Contains(controllerContainerNames, childContainerName) {
+			return fmt.Errorf("controller does not match child containers names")
+		}
+	}
+	return nil
+}
+
+func getChildContainers(child map[string]any) []any {
+	if _, ok := child["spec"].(map[string]any)["containers"]; ok {
+		return child["spec"].(map[string]any)["containers"].([]any)
+	} else if _, ok := child["spec"].(map[string]any)["jobTemplate"]; ok {
+		return child["spec"].(map[string]any)["jobTemplate"].(map[string]any)["spec"].(map[string]any)["containers"].([]any)
+	}
+	return child["spec"].(map[string]any)["template"].(map[string]any)["spec"].(map[string]any)["containers"].([]any)
+}
+
+func getControllerContainers(controller map[string]any) []any {
+	if _, ok := controller["spec"].(map[string]any)["jobTemplate"]; ok {
+		return controller["spec"].(map[string]any)["jobTemplate"].(map[string]any)["spec"].(map[string]any)["template"].(map[string]any)["spec"].(map[string]any)["containers"].([]any)
+	} else {
+		return controller["spec"].(map[string]any)["template"].(map[string]any)["spec"].(map[string]any)["containers"].([]any)
+	}
 }
